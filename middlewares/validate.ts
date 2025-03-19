831057f8f6a55559bf76f61dcf3af5f42f1d7efa
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-03-20 03:17:46 +0700

    feat: hashes redis

--- middlewares/validate.ts
+++ middlewares/validate.ts
@@ -1,11 +1,10 @@
 import type { Request, Response, NextFunction } from "express";
 import { ZodSchema } from "zod";
 
-export const validate = <T>(schema: ZodSchema<T>) => (req: Request, res: Response, next: NextFunction) => {
+export const validate = <T>(schema: ZodSchema<T>): any => (req: Request, res: Response, next: NextFunction) => {
   const result = schema.safeParse(req.body)
   if (!result.success){
-    res.status(400).json({success: false, errors: result.error.errors})
-    return
+    return res.status(400).json({success: false, errors: result.error.errors})
   }
   next();
 }
--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,7 +1,12 @@
 import express from "express";
+import type { Request, Response, NextFunction } from "express";
 import { validate } from "../middlewares/validate.js";
 import { RestaurantShema, type Restaurant } from "../schemas/restaurant.js";
 import { initializeRedisClient } from "../utils/client.js";
+import { nanoid } from "nanoid";
+import { restaurantKeyById } from "../utils/keys.js";
+import { successResponse } from "../utils/responses.js";
+import { checkRestaurantExists } from "../middlewares/checkRestaurantId.js";
 
 const router = express.Router();
 
@@ -9,10 +14,47 @@ router.get("/", async (req, res) => {
   res.send("Hello World!");
 });
 
-router.post("/", validate(RestaurantShema), async (req, res) => {
-  const data = req.body as Restaurant
-  const client = await initializeRedisClient();
-  res.send("Hello World!");
-});
+router.post(
+  "/",
+  validate(RestaurantShema),
+  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+    const data = req.body as Restaurant;
+    try {
+      const client = await initializeRedisClient();
+      const id = nanoid();
+      const restaurantKey = restaurantKeyById(id);
+      const hashData = { id, name: data.name, location: data.location };
+      const addResult = await client.hSet(restaurantKey, hashData);
+      console.log(`Added ${addResult} fields`);
+      return successResponse(res, hashData, "Added new restaurant");
+    } catch (error) {
+      next(error);
+    }
+    res.send("Hello World!");
+  }
+);
+
+router.get(
+  "/:restaurantId",
+  checkRestaurantExists,
+  async (
+    req: Request<{ restaurantId: string }>,
+    res: Response,
+    next: NextFunction
+  ): Promise<any> => {
+    const { restaurantId } = req.params;
+    try {
+      const client = await initializeRedisClient();
+      const restaurantKey = restaurantKeyById(restaurantId);
+      const [_, restaurant] = await Promise.all([
+        client.hIncrBy(restaurantKey, "viewCount", 1),
+        client.hGetAll(restaurantKey),
+      ]);
+      return successResponse(res, restaurant);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 export default router;
